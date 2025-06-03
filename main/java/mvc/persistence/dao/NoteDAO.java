e606d0a8ea0bc6238322f55cccce0cadef2b6a4b
msgggg1 <msggms1@gmail.com>
2025-06-03 11:53:41 +0900

    noteView.jsp
    mvc 패턴 적용
    제목, 글내용, 닉네임, 프로필이미지, 작성일자, 조회수 출력
    main.jsp 각 글에 해당하는 noteView 링크 추가
    로그아웃버튼 js 적용

---
+++ main/java/mvc/persistence/dao/NoteDAO.java
@@ -0,0 +1,26 @@
+package mvc.persistence.dao;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import mvc.domain.dto.NoteDTO;
+import mvc.domain.dto.NoteDetailDTO;
+
+public interface NoteDAO {
+	
+	// 선호 카테고리 - 최신글
+	List<NoteDTO> recentNoteByMyCategory(int categoryIdx, int limit) throws SQLException;
+	
+	// 선호 카테고리 - 인기글
+    List<NoteDTO> popularNoteByMyCategory(int categoryIdx, int limit) throws SQLException;
+    
+    // 전체 카테고리 - 인기글
+	List<NoteDTO> popularNoteByAllCategory(int limit) throws SQLException;
+	
+	// 포스트 뷰 출력
+	NoteDetailDTO printNote(int noteIdx);
+	
+	// 조회수 증가
+	void increaseViewCount(int noteIdx) throws SQLException; 
+	
+}
